ad189427332680db871e014cc2c320c45faf464c
ibrahimdmrck@gmail.com <ibrahimdmrck@gmail.com>
2023-05-03 18:27:54 +0300

    iletşim sayfasından mesaj göndermer işleminde e-posta gönderme işlemi yapıldı

--- contact.php
+++ contact.php
@@ -119,10 +119,10 @@ require 'PHPMailer/src/SMTP.php';
                                     $mail->SMTPAuth=true;
                                     $mail->SMTPSecure="tls"; 
                                     $mail->Port=587;
-                                    $mail->Host="smtp.gmail.com";
-                                    $mail->Username="ibrahimdmrck@gmail.com";
-                                    $mail->Password="vpqsrdpkyvyidtgi";
-                                    $mail->addAddress("ibrahimdmrck@gmail.com");//alıcı adres
+                                    $mail->Host="smtp.example.com";
+                                    $mail->Username="user@example.com'";
+                                    $mail->Password="*****";
+                                    $mail->addAddress("user@example.com'");
                                     $mail->isHTML(true);
                                     $mail->Subject=$subject;
                                     $mail->Body='<html lang="tr">
@@ -133,8 +133,8 @@ require 'PHPMailer/src/SMTP.php';
                                       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
                                     </head>
                                     <body>
-                                    <div class="card text-bg-info mb-3" style="max-width: 18rem;">
-                                    <div class="card-header">'.$name.' - '.$sender.'</div>
+                                    <div class="card text-bg-info mb-3">
+                                    <div class="card-header"><h3>'.$name.' - '.$sender.'</h3></div>
                                     <div class="card-body">
                                         <h5 class="card-title">'.$subject.'</h5>
                                         <p class="card-text">'.$message.'</p>
